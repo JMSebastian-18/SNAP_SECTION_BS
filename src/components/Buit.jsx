9f148ff0a0d5b83f58e081966741f69bdd64b37a
JMSebastian-18 <zpaulina170@gmail.com>
2026-03-09 08:50:09 -0500

    vesion1

--- src/components/Buit.jsx
+++ src/components/Buit.jsx
@@ -26,7 +26,6 @@ export const Buit = () => {
                 </div>
                 <div className='p-5 mb-2 bg-secondary text-white mt-5 border border-5 rounded-5'>
                     <div className="container text-center mt-2">
-                       npm 
                         <h1>Ready to cook smarter?</h1>
                         <p>Hit the button, pick a recipe, and get dinner on the table—fast.</p>
                         <a href='http://www.jango.com' className="btn btn-dark">Browse recipes</a>
